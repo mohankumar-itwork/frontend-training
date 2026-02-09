a3d51c4a4cebd94fbce642fa94b5ebc445f225dc
Mohan Kumar <mohankumar.itwork@gmail.com>
2026-02-09 09:41:12 +0530

    task in js

---
+++ js/classes/05-02-2026.js
@@ -0,0 +1,46 @@
+console.log("Hello, welcome to JavaScript classes!");
+
+var a = 10;
+
+console.log("The value of a is: " + a);
+
+
+// arthmetic operators
+var x = 5;
+var y = 3;
+console.log("x + y = " + (x + y)); // addition
+console.log("x - y = " + (x - y)); // subtraction
+console.log("x * y = " + (x * y)); // multiplication
+console.log("x / y = " + (x / y)); // division
+console.log("x % y = " + (x % y)); // modulus 
+console.log("x ** y = " + (x ** y)); // exponentiation
+
+// assignment operators    
+var z = 10;
+console.log("z = " + z);
+z += 5; // equivalent to z = z + 5
+console.log("z += 5 gives: " + z);
+z -= 3; // equivalent to z = z - 3
+console.log("z -= 3 gives: " + z);
+z *= 2; // equivalent to z = z * 2
+console.log("z *= 2 gives: " + z);
+z /= 4; // equivalent to z = z / 4
+console.log("z /= 4 gives: " + z);
+
+// comparison operators
+var a = 10;
+var b = 20;
+console.log("a == b: " + (a == b)); // equality
+console.log("a != b: " + (a != b)); // inequality
+console.log("a > b: " + (a > b)); // greater than
+console.log("a < b: " + (a < b)); // less than
+console.log("a >= b: " + (a >= b)); // greater than or equal to
+console.log("a <= b: " + (a <= b)); // less than or equal to
+
+// logical operators
+var p = true;
+var q = false;
+console.log("p && q: " + (p && q)); // logical AND
+console.log("p || q: " + (p || q)); // logical OR
+console.log("!p: " + (!p)); // logical NOT  
+
---
+++ js/classes/06-02-2026.js
@@ -0,0 +1,47 @@
+// control statements: if...else, switch, while, do...while, for, for...in, for...of
+
+// if...else
+let age = 20;
+
+console.log("if statement example:");
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+
+// if...else statement
+console.log("if else statement example:");
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// switch statement
+let day = 3;
+
+console.log("switch statement example:");  
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day number.");
+}
